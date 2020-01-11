43d93c746463029d704ecd84e18a627fbe9b590d
Matteo Fusilli <matteofusilli@Matteos-MacBook-Pro.local>
2020-01-11 00:10:58 +0000

    version 1

--- app/src/main/java/com/matthausen/weathermars/MainActivity.java
+++ app/src/main/java/com/matthausen/weathermars/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androdocs.httprequest.HttpRequest;
+import com.darwindeveloper.horizontalscrollmenulibrary.custom_views.HorizontalScrollMenuView;
+import com.darwindeveloper.horizontalscrollmenulibrary.extras.MenuItem;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,11 +19,30 @@ public class MainActivity extends AppCompatActivity {
 
     String API_ENDPOINT = "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0";
 
+    HorizontalScrollMenuView forecast;
+
     TextView avgtemperatureTxt;
     TextView mintemperatureTxt;
     TextView maxtemperatureTxt;
     TextView solTxt;
     TextView seasonTxt;
+
+
+    Double avgTemp;
+    Double minTemp;
+    Double maxTemp;
+    Double minTemp_1_ago;
+    Double maxTemp_1_ago;
+    Double minTemp_2_ago;
+    Double maxTemp_2_ago;
+    Double minTemp_3_ago;
+    Double maxTemp_3_ago;
+    Double minTemp_4_ago;
+    Double maxTemp_4_ago;
+    Double minTemp_5_ago;
+    Double maxTemp_5_ago;
+    Double minTemp_6_ago;
+    Double maxTemp_6_ago;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +56,11 @@ public class MainActivity extends AppCompatActivity {
         seasonTxt = findViewById(R.id.season);
 
         new weatherTask().execute();
+        forecast = findViewById(R.id.forecast);
     }
 
     class weatherTask extends AsyncTask<String, Void, String> {
+
 
         @Override
         protected void onPreExecute() {
@@ -50,41 +73,108 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
+
             try {
                 JSONObject jsonObj = new JSONObject(result);
 
-                 String sol_key = jsonObj.getJSONArray("sol_keys")
-                         .getString(6);
-
-
-                //for(int i = 0; i<jsonObj.length(); i++){
-                  //  Toast.makeText(MainActivity.this, "this" + jsonObj.names().getString(i), Toast.LENGTH_SHORT).show();
-               // }
+                String sol_key = jsonObj.getJSONArray("sol_keys")
+                        .getString(6);
+                String sol_key_1_ago = jsonObj.getJSONArray("sol_keys")
+                        .getString(5);
+                String sol_key_2_ago = jsonObj.getJSONArray("sol_keys")
+                        .getString(4);
+                String sol_key_3_ago = jsonObj.getJSONArray("sol_keys")
+                        .getString(3);
+                String sol_key_4_ago = jsonObj.getJSONArray("sol_keys")
+                        .getString(2);
+                String sol_key_5_ago = jsonObj.getJSONArray("sol_keys")
+                        .getString(1);
+                String sol_key_6_ago = jsonObj.getJSONArray("sol_keys")
+                        .getString(0);
 
                 String season = jsonObj.getJSONObject(sol_key)
                         .getString("Season");
 
-                Double avgTemp = jsonObj.getJSONObject(sol_key)
+                avgTemp = jsonObj.getJSONObject(sol_key)
                         .getJSONObject("AT")
                         .getDouble("av");
 
-                Double minTemp = jsonObj.getJSONObject(sol_key)
+                minTemp = jsonObj.getJSONObject(sol_key)
                         .getJSONObject("AT")
                         .getDouble("mn");
 
-                Double maxTemp = jsonObj.getJSONObject(sol_key)
+                maxTemp = jsonObj.getJSONObject(sol_key)
                         .getJSONObject("AT")
                         .getDouble("mx");
 
-                avgtemperatureTxt.setText(String.valueOf(avgTemp)+ " °C");
-                solTxt.setText(sol_key);
-                seasonTxt.setText(season);
-                mintemperatureTxt.setText("Min: " + String.valueOf(Math.round(minTemp))+ " °C");
-                maxtemperatureTxt.setText("Max: " + String.valueOf(Math.round(maxTemp))+ " °C");
+                minTemp_1_ago = jsonObj.getJSONObject(sol_key_1_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mn");
+
+                maxTemp_1_ago = jsonObj.getJSONObject(sol_key_1_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mx");
+                minTemp_2_ago = jsonObj.getJSONObject(sol_key_2_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mn");
+                maxTemp_2_ago = jsonObj.getJSONObject(sol_key_2_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mx");
+                minTemp_3_ago = jsonObj.getJSONObject(sol_key_3_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mn");
+                maxTemp_3_ago = jsonObj.getJSONObject(sol_key_3_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mx");
+                minTemp_4_ago = jsonObj.getJSONObject(sol_key_4_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mn");
+                maxTemp_4_ago = jsonObj.getJSONObject(sol_key_4_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mx");
+                minTemp_5_ago = jsonObj.getJSONObject(sol_key_5_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mn");
+                maxTemp_5_ago = jsonObj.getJSONObject(sol_key_5_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mx");
+                minTemp_6_ago = jsonObj.getJSONObject(sol_key_6_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mn");
+                maxTemp_6_ago = jsonObj.getJSONObject(sol_key_6_ago)
+                        .getJSONObject("AT")
+                        .getDouble("mx");
+
+                avgtemperatureTxt.setText(Math.round(avgTemp) + " °C");
+                mintemperatureTxt.setText("Min: " + Math.round(minTemp)+ " °C");
+                maxtemperatureTxt.setText("Max: " + Math.round(maxTemp)+ " °C");
+                solTxt.setText("Sol: " + sol_key);
+                seasonTxt.setText("Season: " + season);
+
+                initForecast();
 
             } catch (JSONException e) {
                 Toast.makeText(MainActivity.this, "Could not get a response from NASA: " + e, Toast.LENGTH_LONG).show();
             }
+        }
+
+        private void initForecast() {
+
+            forecast.addItem("Min: " + Math.round(minTemp_1_ago) + " | Max: " + Math.round(maxTemp_1_ago), R.drawable.ic_sun);
+            forecast.addItem("Min: " + Math.round(minTemp_2_ago) + " | Max: " + Math.round(maxTemp_2_ago), R.drawable.ic_sun);
+            forecast.addItem("Min: " + Math.round(minTemp_3_ago) + " | Max: " + Math.round(maxTemp_3_ago), R.drawable.ic_sun);
+            forecast.addItem("Min: " + Math.round(minTemp_4_ago) + " | Max: " + Math.round(maxTemp_4_ago), R.drawable.ic_sun);
+            forecast.addItem("Min: " + Math.round(minTemp_5_ago) + " | Max: " + Math.round(maxTemp_5_ago), R.drawable.ic_sun);
+            forecast.addItem("Min: " + Math.round(minTemp_6_ago) + " | Max: " + Math.round(maxTemp_6_ago), R.drawable.ic_sun);
+
+            forecast.showItems();
+
+            forecast.setOnHSMenuClickListener(new HorizontalScrollMenuView.OnHSMenuClickListener() {
+                @Override
+                public void onHSMClick(MenuItem menuItem, int position) {
+                    Toast.makeText(MainActivity.this, "Weather available", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
